12aef1c4bb97874a92d8eed52279c793aa1ba8e4
Kellen Martin <kema0161@colorado.edu>
2025-01-30 12:01:51 -0600

    Starting homework 1

---
+++ MDO_HW1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from tools import *
+
+def main():
+
+    # Givens 
+    lam = 2         # Convection Coefficient
+    mu = .2         # Diffusion Coefficient
+    ti = 0          # initial time
+    tf = .2         # final time
+    delta_t = .01   # Time step
+    N = int(tf/delta_t)  # Number of time steps
+
+    # Discretized Domain
+    x = np.linspace(0, 1, num=11)
+    U = np.zeros((int(N),11))
+
+    # Inital Conitions
+    U[0,:] = -5*pow(x,2) + 5
+    print(U)
+    # Solve PDE
+
+    # Plot at final time
+    plt.plot(x, U[-1,:], label = 'Solution at tf = 0.2')
+    plt.xlabel('x (postion)')
+    plt.ylabel('U (velocity)')
+    plt.show()
+main()
